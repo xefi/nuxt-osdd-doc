8c66506c135e49ad206b7a596482184e42ceeb67
edepauw <eliott.dpw@gmail.com>
2026-04-01 22:57:30 +0200

    remove table of content for nuxt typecheck

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -7,7 +7,6 @@ definePageMeta({
 })
 
 const route = useRoute()
-const { toc } = useAppConfig()
 const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
 
 const { data: page } = await useAsyncData(route.path, () => queryCollection('docs').path(route.path).first())
@@ -35,20 +34,6 @@ const headline = computed(() => findPageHeadline(navigation?.value, page.value?.
 
 defineOgImageComponent('Docs', {
   headline: headline.value
-})
-
-const links = computed(() => {
-  const links = []
-  if (toc?.bottom?.edit) {
-    links.push({
-      icon: 'i-lucide-external-link',
-      label: 'Edit this page',
-      to: `${toc.bottom.edit}/${page?.value?.stem}.${page?.value?.extension}`,
-      target: '_blank'
-    })
-  }
-
-  return [...links, ...(toc?.bottom?.links || [])].filter(Boolean)
 })
 </script>
 
@@ -80,35 +65,5 @@ const links = computed(() => {
 
       <UContentSurround :surround="surround" />
     </UPageBody>
-
-    <template
-      v-if="page?.body?.toc?.links?.length"
-      #right
-    >
-      <UContentToc
-        :title="toc?.title"
-        :links="page.body?.toc?.links"
-      >
-        <template
-          v-if="toc?.bottom"
-          #bottom
-        >
-          <div
-            class="hidden lg:block space-y-6"
-            :class="{ '!mt-6': page.body?.toc?.links?.length }"
-          >
-            <USeparator
-              v-if="page.body?.toc?.links?.length"
-              type="dashed"
-            />
-
-            <UPageLinks
-              :title="toc.bottom.title"
-              :links="links"
-            />
-          </div>
-        </template>
-      </UContentToc>
-    </template>
   </UPage>
 </template>
